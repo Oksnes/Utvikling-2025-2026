2fe341ca269ab82ea17dbbe1664eb99e665025c5
Oksnes <timmen08@outlook.com>
2025-09-12 12:32:15 +0200

    Matbutikk

--- Matbutikk/code.js
+++ Matbutikk/code.js
@@ -10,25 +10,30 @@ let varer = [
     { navn: "Agurk", pris: 12.90, bilde: "mat_14.png", handlekurv: 0 },
     { navn: "Paprika", pris: 13.30, bilde: "mat_05.png", handlekurv: 0 },
     { navn: "Potet", pris: 3.90, bilde: "mat_13.png", handlekurv: 0 },
-    { navn: "Tomat", pris: 4.90, bilde: "mat_08.png", handlekurv: 0 }
+    { navn: "Tomat", pris: 4.90, bilde: "tomatBilde.png", handlekurv: 0 },
+    { navn: "Fisk", pris: 25.90, bilde: "fiskBilde.png", handlekurv: 0 },
+    { navn: "Brød", pris: 30.30, bilde: "brodBilde.png", handlekurv: 0 },
+    { navn: "Kylling", pris: 25.90, bilde: "kyllingBilde.png", handlekurv: 0 },
+    { navn: "Banan", pris: 15.90, bilde: "bananBilde.png", handlekurv: 0 },
+    { navn: "Melk", pris: 20.90, bilde: "melkBilde.png", handlekurv: 0 },
 ];
 
-for (let vare of varer) {
+for (let i = 0; i < varer.length; i++) {
     let varediv = document.createElement("div");
     varediv.className = "vare"
     
     let vareOverskrift = document.createElement("h3");
-    vareOverskrift.innerText = varer[vare].navn;
+    vareOverskrift.innerText = varer[i].navn;
 
     let vareBilde = document.createElement("img");
-    vareBilde.src = "medier/" + varer[vare].bilde;
+    vareBilde.src = "medier/" + varer[i].bilde;
 
     let varePris = document.createElement("p");
-    varePris.innerText = varer[vare].pris +" kr";
+    varePris.innerText = varer[i].pris +"0 kr";
 
     let vareKnapp = document.createElement("button");
     vareKnapp.innerText = "Kjøp";
-    vareKnapp.id = varer[vare].navn;
+    vareKnapp.id = i;
     vareKnapp.addEventListener("click", kjopVare);
 
     varediv.appendChild(vareOverskrift);
@@ -39,8 +44,8 @@ for (let vare of varer) {
     varerHtml.appendChild(varediv);
 }
 
-function kjopVare(event) {
-    let varenummer = event.target.id;
+function kjopVare(e) {
+    let varenummer = e.target.id;
 
     varer[varenummer].handlekurv++;
 
@@ -52,13 +57,13 @@ function oppdaterHandlekurv() {
 
     handlekurvHtml.innerHTML = "<h3> Handlekurv </h3>";
 
-    for (let vare of varer) {
-        if (varer[vare].handlekurv > 0) {
-            let varepris = varer[vare].pris * varer[vare].handlekurv;
+    for (let i = 0; i< varer.length; i++) {
+        if (varer[i].handlekurv > 0) {
+            let varepris = varer[i].pris * varer[i].handlekurv;
 
             totalpris += varepris;
 
-            handlekurvHtml.innerHTML += "<p>" + varer[vare].handlekurv + " x " + varer[vare].navn + "(" + varepris.toFixed(2) + " kr)</p>";
+            handlekurvHtml.innerHTML += "<p>" + varer[i].handlekurv + " x " + varer[i].navn + "(" + varepris.toFixed(2) + " kr)</p>";
         }
     }
 
