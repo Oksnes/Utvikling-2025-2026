91a854a74f13515acf52237fbf6ef54a5a56eab6
Oksnes <timmen08@outlook.com>
2025-10-03 11:57:46 +0200

    NodeChat 0.54321.1

--- Chat/app.js
+++ Chat/app.js
@@ -10,6 +10,10 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
+// Legg til body-parsing for skjema/JSON
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     // res.send('Hello World!');
     res.sendFile(__dirname + '/public/index.html');
@@ -20,7 +24,23 @@ app.get('/hentMeldinger', (req, res) => {
     const row = db.prepare('SELECT * FROM Melding').all();
     res.json(row);
 });
-    
+
+app.post('/leggMelding', (req, res) => {
+  try {
+    let { navn, melding, tid } = req.body;
+    navn = navn.toString().trim();
+    melding = melding.toString().trim();
+    tid = tid.toString().trim();
+
+    db.prepare('INSERT INTO melding (Navn, Innhold, Tid) VALUES (?, ?, ?)')
+    .run(navn, melding, tid);
+    return res.sendStatus(201)
+  } 
+  catch (err) {
+    console.error('Feil ved innsending av melding:', err);
+    return res.status(500).json({ error: 'Kunne ikke lagre melding' });
+  }
+});
 
 
 //åpner port på serveren
